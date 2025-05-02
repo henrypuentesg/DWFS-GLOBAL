d556da24b4658f32ae4a361fda350b210e093bc8
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2025-05-02 14:43:26 +0200

    Minor changes

--- Tema_5/Codigo de ejemplo/hooks-router-ejemplo/src/components/Cuadrado.jsx
+++ Tema_5/Codigo de ejemplo/hooks-router-ejemplo/src/components/Cuadrado.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import {useLocation, useParams, useSearchParams} from "react-router-dom";
 
 export const Cuadrado = () => {
+
+  //Obtener la ubicación actual
   const location = useLocation();
   console.log(location);
+
+  //Obtener los path params
   const { id } = useParams();
 
+  //Obtener los query params "artesanal"
   const queryParams = new URLSearchParams(location.search);
   const paramValue = queryParams.get("param");
   console.log(paramValue);
-  
-  return <div className="cuadrado">Cuadrado {id}</div>;
+
+    //Obtener los query params con hook
+  const queryParamsMap = useSearchParams();
+
+  return <div className="cuadrado">Cuadrado {id} y query params: {queryParamsMap}</div>;
 };
